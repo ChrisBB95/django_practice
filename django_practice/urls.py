be07f187bf8d311868c8f5c867c66695aff8833f
Christopher Basketfield <chrisBB@ak.net>
2019-01-28 14:31:16 -0900

    added form for creating products

--- django_practice/urls.py
+++ django_practice/urls.py
@@ -18,12 +18,13 @@ from django.urls import path
 
 # own
 from pages.views import home_view, about_view
-from products.views import product_detail_view
+from products.views import product_detail_view, product_create_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
     path('home/', home_view, name='home'),
     path('about/', about_view, name='about'),
-    path('product/', product_detail_view, name='product')
+    path('product/', product_detail_view, name='product'),
+    path('create/', product_create_view, name='create')
 ]
--- products/views.py
+++ products/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
 from .models import Product
+from .forms import ProductForm
+
 # Create your views here.
 
 
 def product_detail_view(request):
 
     obj = Product.objects.get(id=4)
-
     context = {
         'obj': obj
     }
-
     return render(request, "products/product_detail.html", context)
+
+
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, "products/product_create.html", context)
