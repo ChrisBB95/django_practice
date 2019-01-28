a5334ba025bc9c99ce15f335cbd73e4e410bdace
Christopher Basketfield <chrisBB@ak.net>
2019-01-28 13:30:36 -0900

    started using template context

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def home_view(request):
     return render(request, 'home.html', {})
 
+
 def about_view(request):
-    return render(request, 'about.html', {})
+    context = {
+        "text": "squeedilyspooch",
+        "number": 80085
+    }
+    return render(request, 'about.html', context)
